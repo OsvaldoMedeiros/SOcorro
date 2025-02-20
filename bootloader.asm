6aa7baa20f53c7f855a93018314023a40b9e5967
Anderson Moura <47828681+andersonmoura-cn@users.noreply.github.com>
2025-02-20 10:05:03 -0300

    Update bootloader.asm
    nasm

--- bootloader.asm
+++ bootloader.asm
@@ -1,44 +1,39 @@
-; Bootloader.asm
-; Compatível com MASM 6.14
+bits 16
+org 0x7C00      ; Define o endereço de carregamento como 7C00h
 
-.model small
-.stack 100h
-.code
-
-org 7C00h  ; Define o endereço de origem como 7C00h
+%define SETOR_KERNEL 3
 
 start:
     ; Inicialização da pilha
-    mov ax, 07C0h
+    mov ax, 0x07C0  
     mov ss, ax
-    mov sp, 03FEh
+    mov sp, 0x03FE
 
     ; Configuração do segmento de dados
     xor ax, ax
     mov ds, ax
 
     ; Alterar o modo de vídeo
-    mov ah, 00h
-    mov al, 03h
-    int 10h
+    mov ah, 0x00
+    mov al, 0x03
+    int 0x10
 
     ; Ler dados do disquete (setor 2)
-    mov ah, 02h
-    mov al, 1
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    mov dl, 0
-    mov bx, 0800h
+    mov ah, 0x02
+    mov al, 0x05        ; ler 5 setores
+    mov ch, 0x00
+    mov cl, SETOR_KERNEL 
+    mov dh, 0x00
+    mov dl, 0x00
+    mov bx, 0x0800
     mov es, bx
-    mov bx, 0
-    int 13h
+    mov bx, 0x0000
+    int 0x13
 
     ; Pular para o kernel
-    jmp 0800h:0000h
+    jmp 0x0800:0x0000
 
-    ; Preencher o restante do setor de boot
-    times 510-($-start) db 0
-    dw 0xAA55  ; Assinatura de bootloader
+; Preencher o setor de boot com zeros até o byte 510
+times 510 - ($ - $$) db 0
+dw 0xAA55      ; Assinatura de bootloader
 
-end start
